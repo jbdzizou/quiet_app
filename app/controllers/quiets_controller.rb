bdce60abd9d4644088c61fda02e231c1424c1915
jbdzizou <jbdzizou@gmail.com>
2019-11-13 21:10:07 +0900

    Add simple_form and create action

--- app/controllers/quiets_controller.rb
+++ app/controllers/quiets_controller.rb
@@ -1,18 +1,32 @@
 class QuietsController < ApplicationController
+  /before_action :authenticate_user!, only: [:show, :new]/
+  
   def index
-    @quiets = Quiet.all 
+    @quiets = Quiet.all.order("created_at DESC")
   end
 
   def show 
   end
 
   def new
-    @quiet = current_user.quiet.build
+    @quiet = Quiet.new
   end
 
   def create
+    @quiet = Quiet.new(quiet_params)
+    if @quiet.save
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def edit
   end
+end
+
+private
+
+def quiet_params
+  params.require(:quiet).permit(:name, :description)
 end
