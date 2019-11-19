85020068dc12da26cf12abbd133d803a5ac0f3da
jbdzizou <jbdzizou@gmail.com>
2019-11-19 21:22:51 +0900

    Add edit page nad more

--- app/controllers/quiets_controller.rb
+++ app/controllers/quiets_controller.rb
@@ -1,12 +1,12 @@
 class QuietsController < ApplicationController
   /before_action :authenticate_user!, only: [:show, :new]/
+  before_action :find_quiet, only: [:show, :edit, :update, :destroy]
 
   def index
     @quiets = Quiet.all.order("created_at DESC")
   end
 
   def show
-    @quiet = Quiet.find(params[:id])
   end
 
   def new
@@ -26,15 +26,26 @@ class QuietsController < ApplicationController
   end
 
   def update
+    if @quiet.update(quiet_params)
+      redirect_to quiet_path
+    else
+      render 'edit'
+    end
   end
 
   def destroy
+    @quiet.destroy
+    redirect_to root_path
   end
 
   private
 
   def quiet_params
     params.require(:quiet).permit(:name, :description)
+  end
+
+  def find_quiet
+    @quiet = Quiet.find(params[:id])
   end
 
 end
