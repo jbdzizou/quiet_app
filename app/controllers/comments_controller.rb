9c4a764da2731c2cf8f60ecc6b3e4577ff5ed5b6
jbdzizou <jbdzizou@gmail.com>
2019-11-22 21:48:13 +0900

    Update Comment controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,10 @@
 class CommentsController < ApplicationController
+    before_action :find_quiet, only: [:create, :edit, :update, :destroy]
+    before_action :find_comment, only: [:update, :destroy]
 
     def create
-        @quiet = Quiet.find(params[:quiet_id])
         @comment = @quiet.comments.create(comment_params)
-        @commnet.user_id = current_user.id
+        @comment.user_id = current_user.id
 
         if @comment.save
             redirect_to quiet_path(@quiet)
@@ -12,9 +13,20 @@ class CommentsController < ApplicationController
         end
     end
 
+    def edit
+    end
+
     private
 
     def comment_params
-        params.require.(:comment).permit(:content)
+        params.require(:comment).permit(:content)
+    end
+
+    def find_quiet
+        @quiet = Quiet.find(params[:quiet_id])
+    end
+
+    def find_comment
+        @comment = @quiet.comments.find(params[:id])
     end
 end
