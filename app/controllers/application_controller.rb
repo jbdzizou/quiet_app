8be409c4abb226dcf521e3f065ca9ef448a8922e
jbdzizou <jbdzizou@gmail.com>
2019-11-25 19:21:17 +0900

    Add Users index pages

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,21 @@
 class ApplicationController < ActionController::Base
+    # deviseコントローラーにストロングパラメータを追加する    
     before_action :configure_permitted_parameters, if: :devise_controller?
 
+    # ログイン済ユーザーのみにアクセスを許可する
+    #before_action :authenticate_user!
+    
     protected
 
     def configure_permitted_parameters
         added_attrs = [:name, :email, :password, :password_confirmation, :remember_me]
         devise_parameter_sanitizer.permit :sign_up, keys: added_attrs
         devise_parameter_sanitizer.permit :account_update, keys: added_attrs
+    end
+
+    # ログイン後、blogs/indexに移動する
+    def after_sign_in_path_for(resource)
+        quiets_path
     end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/show'
+  resources :users, only: [:index]
+
+  # ログイン、アカウント編集後、任意のページに推移させるための記述
+  devise_for :users,controllers: {
+    registrations: 'users/registrations'
+  }
   resources :quiets do
     resources :comments
   end
