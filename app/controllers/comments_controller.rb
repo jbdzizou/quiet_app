8516948ad39668d1a5f67a06c3544c5307f86451
jbdzizou <jbdzizou@gmail.com>
2019-12-06 21:00:16 +0900

    Comment's update

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,10 @@
 class CommentsController < ApplicationController
-    before_action :find_quiet, only: [:create, :edit, :update, :destroy]
-    before_action :find_comment, only: [:update, :destroy]
+    before_action :find_quiet, only: [:new, :create, :edit, :update, :destroy]
+    before_action :find_comment, only: [:edit, :update, :destroy]
+
+    def new
+        @comment = Comment.new
+    end
 
     def create
         @comment = @quiet.comments.create(comment_params)
@@ -9,12 +13,26 @@ class CommentsController < ApplicationController
         if @comment.save
             redirect_to quiet_path(@quiet)
         else
-            render 'edit'
+            render 'new'
         end
     end
 
     def edit
     end
+
+    def update
+        if @comment.update(find_comment)
+            redirect_to quiet_path(@quiet)
+        else
+            render 'edit'
+        end
+    end
+    
+    def destroy
+        @comment.destroy
+        redirect_to quiet_path(@quiet)
+    end
+    
 
     private
 
