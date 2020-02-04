a630da3d1044c3e37abfcc9564fd8e913d5e99d8
jbdzizou <jbdzizou@gmail.com>
2020-02-04 17:45:45 +0900

    Fix carrierwave 0204-1

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -2,20 +2,16 @@ require 'carrierwave/storage/abstract'
 require 'carrierwave/storage/file'
 require 'carrierwave/storage/fog'
 
-CarrierWave.configure do |config|
 
-    if Rails.env.production?
-        config.storage :fog
-        config.fog_provider = 'fog/aws'
-        config.fog_directory  = 'rails-quiet-photo'
+if Rails.env.production?
+    CarrierWave.configure do |config|
         config.fog_credentials = {
         provider: 'AWS',
         aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
         aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
         region: ENV['AWS_DEFAULT_REGION']
         }
-    else
-        config.storage :file
+        config.fog_directory  = 'rails-quiet-photo'
     end
 end
 # 日本語ファイル名の設定
